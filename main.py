dfa21db15d60ea2255735ebf0d5f0f847d95d3dd
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-02-10 18:05:12 +0800

    Added: Conditions for row and column in range 1-10

--- main.py
+++ main.py
@@ -15,4 +15,19 @@ class Solver:
         self.cells = {}
         self.entries = []
 
-        
+        for row in range(1, 10):
+            for column in range (1, 10):
+                if ((row in (1,2,3,7,8,9) and column in (4,5,6)) or (row in (4,5,6) and column in (1,2,3,7,8,9))):
+                    color='gray20'
+                else:
+                    color='gray80'
+                cell = tkinter.Frame(master, highlightbackground=color, highlightcolor=color, highlightthickness=2, width=50, height=50, padx=3,  pady=3, background='black')
+                cell.grid(row=row, column=column)
+                self.cells[(row, column)] = cell
+
+                e = tkinter.Entry(self.cells[row, column], justify='center')
+                e.place(height=40, width=40)
+                
+                self.entries.append(e)
+
+                
